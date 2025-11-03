c20f40f420d2b46ebe9c5b87e3b0c1d750e82098
Joana <joanadasilva.pr@gmail.com>
2025-11-03 20:13:17 -0300

    base principal

--- Front.java
+++ Front.java
@@ -1,12 +1,29 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Front {
     public Front(){
-        JFrame jFrame=new JFrame();
-        jFrame.setVisible(true);
-        jFrame.setLocation(350,70);
-        jFrame.setSize(800,600);
-        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JFrame janela_principal=new JFrame();
 
+        janela_principal.setBounds(400,100,730,460);
+        janela_principal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        janela_principal.setLayout(null);
+        janela_principal.setContentPane(new JLabel(new ImageIcon("#cat #wallpaper #pc.jpg")));
+
+        JLabel tema = new JLabel("Tech Music");
+        tema.setBounds(150,32,400,76);
+        tema.setFont(new Font("SansSerif", Font.BOLD, 42));
+        janela_principal.add(tema);
+
+        JButton botao_usuario=new JButton("Usuario");
+        botao_usuario.setBounds(230,150,275,50);
+        janela_principal.add(botao_usuario);
+
+        JButton botao_playlist =new JButton("Playlists");
+        botao_playlist.setBounds(230,210,275,50);
+        janela_principal.add(botao_playlist);
+
+
+        janela_principal.setVisible(true);
     }
 }
