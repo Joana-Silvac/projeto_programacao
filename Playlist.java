af9763591913316a754007cd61ed54321b8d42f8
Crislany Victória <crislanyviictoria@gmail.com>
2025-11-17 20:26:36 -0300

    classe responsável por playlist

--- Playlist.java
+++ Playlist.java
@@ -1,2 +1,38 @@
-public class Playlist {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Playlist{
+    private int id;
+    private String nome;
+    private List<Musica>musicas;
+
+    public Playlist(int id, String nome){
+        this.id =id;
+        this.nome =nome;
+        this.musicas =new ArrayList<>();
+    }
+
+    public void adicionarMusica(Musica musica){
+        musicas.add(musica);
+    }
+
+    public void removerMusica(Musica musica){
+        musicas.remove(musica);
+    }
+
+    public int getDuracaoTotal(){
+        int total =0;
+        for (Musica musica:musicas) {
+            total+= musica.getDuracao();
+        }
+        return total;
+    }
+
+    public int getId(){return id; }
+    public String getNome(){return nome;}
+    public List<Musica>getMusicas(){ return new ArrayList<>(musicas); }
+
+    public String toString(){
+        return String.format("Playlist [%d] %s | %d músicas", id, nome, musicas.size());
+    }
 }
